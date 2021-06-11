6473832bff8229eb2f7cdc9cc873692bc0df1219
Moymat <deckertmichel@gmail.com>
2021-06-11 15:30:33 +0200

    implemented goals section

---
+++ components/Goals.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+import styles from "../styles/Goals.module.css";
+import { addScrollEvent } from "../utils/addScrollEvent";
+
+export default function Goals() {
+	const title = useRef();
+	const info1 = useRef();
+	const info2 = useRef();
+
+	useEffect(() => {
+		addScrollEvent(title.current);
+		addScrollEvent(info1.current);
+		addScrollEvent(info2.current);
+	}, []);
+
+	return (
+		<section className={`${styles.section} section`}>
+			<h2 ref={title} className={`${styles.title} secondary-title`}>
+				Objectifs principaux
+			</h2>
+			<div ref={info1} className={styles.info1}>
+				<p className={styles.num}>1</p>
+				<p className={styles.text}>
+					Créer un environnement confortable et améliorer la qualité du service
+					client
+				</p>
+			</div>
+			<div ref={info2} className={styles.info2}>
+				<p className={styles.num}>2</p>
+				<p className={styles.text}>
+					Améliorer constamment la qualité des services fournis par formation du
+					personnel, achat de nouvel équipement et publicité améliorée pour le
+					marché
+				</p>
+			</div>
+		</section>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
-import Head from 'next/head';
-import Hero from '../components/Hero';
-import About from '../components/About';
+import Head from "next/head";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Goals from "../components/Goals";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Digital Project</title>
-      </Head>
-      <Hero />
-      <About />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Digital Project</title>
+			</Head>
+			<Hero />
+			<About />
+			<Goals />
+		</>
+	);
 }
