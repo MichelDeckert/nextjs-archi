042a6c168d51b923b321e5df43673124909b13d5
Moymat <deckertmichel@gmail.com>
2021-06-11 16:10:32 +0200

    added projects section and fixed footer width

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Goals from "../components/Goals";
+import Projects from "../components/Projects";
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 			<Hero />
 			<About />
 			<Goals />
+			<Projects />
 		</>
 	);
 }
--- utils/addScrollEvent.js
+++ utils/addScrollEvent.js
@@ -1,23 +1,23 @@
 function isClose(element) {
-  const windowScrollY = window.scrollY;
-  const clientHeight = document.scrollingElement.clientHeight;
+	const windowScrollY = window.scrollY;
+	const clientHeight = document.scrollingElement.clientHeight;
 
-  return windowScrollY + clientHeight - element.offsetTop >= clientHeight * 0.3;
+	return windowScrollY + clientHeight - element.offsetTop >= clientHeight * 0.1;
 }
 
 function showElement(element, delay, cb) {
-  if (isClose(element)) {
-    setTimeout(() => {
-      element.style.transform = 'translate(0, 0)';
-      element.style.opacity = 1;
-    }, delay);
-    document.removeEventListener('scroll', cb);
-  }
+	if (isClose(element)) {
+		setTimeout(() => {
+			element.style.transform = "translate(0, 0)";
+			element.style.opacity = 1;
+		}, delay);
+		document.removeEventListener("scroll", cb);
+	}
 }
 
 export function addScrollEvent(element, delay = 10) {
-  document.addEventListener('scroll', function callback() {
-    showElement(element, delay, callback);
-  });
-  document.dispatchEvent(new CustomEvent('scroll'));
+	document.addEventListener("scroll", function callback() {
+		showElement(element, delay, callback);
+	});
+	document.dispatchEvent(new CustomEvent("scroll"));
 }
