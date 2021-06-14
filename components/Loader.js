700e681fe0a7448e434d3601fd7dd260cc1e02b7
Moymat <deckertmichel@gmail.com>
2021-06-14 23:14:46 +0200

    refactor loader

--- components/Loader.js
+++ components/Loader.js
@@ -3,8 +3,6 @@ import React from "react";
 export default function Loader() {
 	return (
 		<div className="loader-container">
-			<div className="mask"></div>
-			<div className="back"></div>
 			<div className="loader"></div>
 		</div>
 	);
