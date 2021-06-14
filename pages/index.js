6e355a0b43b05e619f87a26d79241827b907ed14
Moymat <deckertmichel@gmail.com>
2021-06-14 20:43:30 +0200

    home page waits for images to load

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,45 @@
-import Head from 'next/head';
-import Hero from '../components/index/Hero';
-import About from '../components/index/About';
-import Goals from '../components/index/Goals';
-import Projects from '../components/index/Projects';
-import Form from '../components/index/Form';
+import { useState } from "react";
+import Head from "next/head";
+import Hero from "../components/index/Hero";
+import About from "../components/index/About";
+import Goals from "../components/index/Goals";
+import Projects from "../components/index/Projects";
+import Form from "../components/index/Form";
 
 export default function Home({ projects }) {
-  return (
-    <>
-      <Head>
-        <title>Digital Project</title>
-      </Head>
-      <Hero
-        projects={projects.filter(project => project.showcased === 'HERO')}
-      />
-      <About />
-      <Goals />
-      <Projects
-        projects={projects
-          .filter(project => project.showcased === 'PROJECTS')
-          .slice(0, 5)}
-      />
-      <Form />
-    </>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	return (
+		<>
+			<Head>
+				<title>Digital Project</title>
+			</Head>
+			<Hero
+				isLoading={isLoading}
+				setIsLoading={setIsLoading}
+				projects={projects.filter(project => project.showcased === "HERO")}
+			/>
+			{!isLoading && (
+				<>
+					<About />
+					<Goals />
+					<Projects
+						projects={projects
+							.filter(project => project.showcased === "PROJECTS")
+							.slice(0, 5)}
+					/>
+					<Form />
+				</>
+			)}
+		</>
+	);
 }
 
 export async function getStaticProps() {
-  const data = await import('./api/db.json');
+	const data = await import("./api/db.json");
 
-  return {
-    props: {
-      projects: data.projects
-    }
-  };
+	return {
+		props: {
+			projects: data.projects,
+		},
+	};
 }
