4cd64b84a1b6681783b5c2969fad167f3932af88
Moymat <36814609+Moymat@users.noreply.github.com>
2021-06-16 15:45:23 +0200

    Update Loader.js

--- components/Loader.js
+++ components/Loader.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 export default function Loader() {
-	return (
-		<div className="loader-container">
-			<div className="loader"></div>
-		</div>
-	);
+  return (
+    <div className="loader-background">
+      <div className="loader-container">
+        <div className="loader" />
+      </div>
+    </div>
+  );
 }
