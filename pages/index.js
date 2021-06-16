af72d4b39c25229110588b69922acb04ec8979f7
Moymat <36814609+Moymat@users.noreply.github.com>
2021-06-16 16:55:48 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home({ projects }) {
       <Head>
         <title>Digital Project</title>
       </Head>
-      <Loader />
+      {isLoading && <Loader />}
       <Hero
         projects={projects.filter(project => project.showcased === 'HERO')}
         isLoading={isLoading}
