c267db3ac5da190dea20d3026a012722d271cc8c
Moymat <36814609+Moymat@users.noreply.github.com>
2021-06-16 15:48:49 +0200

    Update Loader.js

--- components/Loader.js
+++ components/Loader.js
@@ -4,7 +4,7 @@ export default function Loader() {
   return (
     <div className="loader-background">
       <div className="loader-container">
-        <div className="loader"><div/>
+        <div className="loader"></div>
       </div>
     </div>
   );
