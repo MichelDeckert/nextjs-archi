ab4632e3ff87f197080b621f74f080ecd67fc736
Moymat <36814609+Moymat@users.noreply.github.com>
2021-06-16 14:54:19 +0200

    Update Hero.js

--- components/index/Hero.js
+++ components/index/Hero.js
@@ -31,7 +31,7 @@ export default function Hero({ projects, isLoading, setIsLoading }) {
       setSlideToShow(0);
       setTimeout(() => {
         setIsLoading(false);
-      }, 800);
+      }, 400);
     }
   }, [areImagesLoaded]);
 
